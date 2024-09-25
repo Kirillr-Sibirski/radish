30127debdc01698185db070d748569b5481640be
Tymur-Tykva <tykva.tymur@gmail.com>
2024-09-25 23:03:13 +0200

    Added getLoan transaction manifest generator

--- my-app/app/manifests.tsx
+++ my-app/app/manifests.tsx
@@ -1,45 +1,50 @@
-export function generateEstimateLoan(component_address: String, collateral: Map<String, Number>) {
-  // CALL_METHOD
-  //   Address("${component_address}")
-  //   "lock_fee"
-  //   Decimal("5");  
-  let s1 = `
+export function generateEstimateLoan(
+  component_address: string,
+  collateral: Map<string, number>
+) {
+  const s1 = `
     CALL_METHOD
       Address("${component_address}")
       "estimate_loan"
-      Map<Address, Decimal>(`
-  
-  let s2 = ""
+      Map<Address, Decimal>(`;
+
+  let s2 = "";
   collateral.forEach((decimal, address) => {
-    s2 += `Address("${address}") => Decimal("${decimal}"), `
-  })
+    s2 += `Address("${address}") => Decimal("${decimal}"), `;
+  });
 
-  return s1 + s2 + ");"
+  return s1 + s2 + ");";
+}
 
+export function generateGetLoan(
+  account_address: string,
+  component_address: string,
+  collateral: Map<string, number>
+) {
+  let s_resource = "";
+  collateral.forEach((amount, address) => (
+    s_resource += `CALL_METHOD
+    Address("${account_address}")
+    "withdraw"
+    Address("${address}")
+    Decimal("${amount}");
+  TAKE_FROM_WORKTOP
+    Address("${address}")
+    Decimal("${amount}")
+    Bucket("Bucket_${address}");
+    
+  `));
+  let s_bucket = "";
+  collateral.forEach((_, address) => (s_bucket += `Bucket(Bucket_${address}), `));
 
-  
-  // return `
-  //   CALL_METHOD
-  //     Address("${component_address}")
-  //     "estimate_loan"
-  //     Map<Address, Decimal>(${() => {
-  //     let s = '';
-  //     collateral.forEach((address, decimal) => {
-  //       s += `Address("${address}") => Decimal("${decimal}"), `
-  //     })
-  //     return s
-  //   }
-  //   });
-  //   `
+  return `${s_resource}CALL_METHOD
+    Address("${component_address}")
+    "get_loan"
+    Array<Bucket>(${s_bucket});
 
-  //   CALL_METHOD
-  //   Address("component_tdx_2_1crxwj8t0y54sk4kyfl6xlxhzff7tfjecqetexeqhc29xp5vydtcj3u")
-  //   "estimate_loan"
-  //   Map<Address, Decimal>(()=>{
-  //     let s = "";
-  //     collateral.forEach((address, decimal)=>{
-  //         s += 'Address("'.concat(address, '") => Decimal("').concat(decimal, '"), ');
-  //     });
-  //     return s;
-  // });
+  CALL_METHOD
+    Address("${account_address}")
+    "deposit_batch"
+    Expression("ENTIRE_WORKTOP");
+`;
 }
