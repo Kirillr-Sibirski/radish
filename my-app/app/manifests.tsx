fbf4bf9b87891007d98e0274b1748b254db72346
Tymur-Tykva <tykva.tymur@gmail.com>
2024-09-26 12:28:17 +0200

    Added manifest generator for estimate_repay

--- my-app/app/manifests.tsx
+++ my-app/app/manifests.tsx
@@ -22,8 +22,9 @@ export function generateGetLoan(
   collateral: Map<string, number>
 ) {
   let s_resource = "";
-  collateral.forEach((amount, address) => (
-    s_resource += `CALL_METHOD
+  collateral.forEach(
+    (amount, address) =>
+      (s_resource += `CALL_METHOD
     Address("${account_address}")
     "withdraw"
     Address("${address}")
@@ -33,9 +34,12 @@ export function generateGetLoan(
     Decimal("${amount}")
     Bucket("Bucket_${address}");
     
-  `));
+  `)
+  );
   let s_bucket = "";
-  collateral.forEach((_, address) => (s_bucket += `Bucket("Bucket_${address}"), `));
+  collateral.forEach(
+    (_, address) => (s_bucket += `Bucket("Bucket_${address}"), `)
+  );
 
   return `${s_resource}CALL_METHOD
     Address("${component_address}")
@@ -47,4 +51,31 @@ export function generateGetLoan(
     "deposit_batch"
     Expression("ENTIRE_WORKTOP");
 `;
+}
+
+export function generateEstimateRepay(
+  account_address: string,
+  component_address: string,
+  borrower_badge_address: string,
+  borrower_badge_id: string
+) {
+  return `CALL_METHOD
+    Address("${account_address}")
+    "create_proof_of_non_fungibles"
+    Address("${borrower_badge_address}")
+    Array<NonFungibleLocalId>(NonFungibleLocalId("${borrower_badge_id}"));
+
+  POP_FROM_AUTH_ZONE 
+    Proof("Proof_${borrower_badge_id}");
+
+  CALL_METHOD
+    Address("${component_address}")
+    "estimate_repay"
+    Proof("Proof_${borrower_badge_id}")
+    Decimal("0.6");
+
+  CALL_METHOD
+    Address("${account_address}")
+    "deposit_batch"
+    Expression("ENTIRE_WORKTOP");`;
 }
